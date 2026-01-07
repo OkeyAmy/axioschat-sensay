66e250b4c39297f28e3d80dbc2efb0c187fedb1f
Okey Amy <amaobiokeoma@gmail.com>
2026-01-07 01:37:58 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
 import { componentTagger } from "lovable-tagger"
-import { nodePolyfills } from "vite-plugin-node-polyfills"
+
 import { fileURLToPath } from 'url'
 
 // ESM compatible __dirname alternative
@@ -52,10 +52,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === "development" && componentTagger(),
-    nodePolyfills({
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    }),
   ].filter(Boolean),
   resolve: {
     alias: {
